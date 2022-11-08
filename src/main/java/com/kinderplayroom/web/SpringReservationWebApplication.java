ae1af8bbc228ca27049c6ad406f5b773f7bc3319
이현수 <ihyeonsu@ihyeonsuui-iMac.local>
2022-11-08 18:34:20 +0900

    First commit

---
+++ src/main/java/com/kinderplayroom/web/SpringReservationWebApplication.java
@@ -0,0 +1,13 @@
+package com.kinderplayroom.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReservationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReservationWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinderplayroom/web/SpringReservationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinderplayroom.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReservationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
