a24a1bcae5a607d3d09142b81906e97d86f25214
이현수 <ihyeonsu@ihyeonsuui-iMac.local>
2022-11-12 16:48:28 +0900

    Add tiles definition 'index' index 페이지를 tiles에 추가함

---
+++ src/main/java/com/springreservation/web/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.springreservation.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+
+public class WebConfig {
+    
+    @Bean
+    TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[]{"WEB-INF/tiles.xml"});
+        tilesConfigurer.setCheckRefresh(true);
+
+        return tilesConfigurer;
+    }
+
+    @Bean
+    TilesViewResolver tilesViewResolver() {
+
+        TilesViewResolver tilesViewResolver = new TilesViewResolver();
+        tilesViewResolver.setViewClass(TilesView.class);
+        tilesViewResolver.setOrder(1);
+        return tilesViewResolver;
+    }
+}
