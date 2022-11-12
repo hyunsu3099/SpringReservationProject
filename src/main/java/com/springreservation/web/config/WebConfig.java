6dd9a6bfaff8865429008fed545064655ab0669f
이현수 <ihyeonsu@ihyeonsuui-iMac.local>
2022-11-12 18:59:10 +0900

    Add @Configuration Annoration to WebConfig WebConfig.java 클래스에 어노테이션을 입력

--- src/main/java/com/springreservation/web/config/WebConfig.java
+++ src/main/java/com/springreservation/web/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.springreservation.web.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 
+@Configuration
 public class WebConfig {
     
     @Bean
